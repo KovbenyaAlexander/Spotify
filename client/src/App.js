539486379e7377ac2f32c474c953d734bb228d89
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-05-18 20:05:13 +0300

    add authorization

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Login from "./Components/Login";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/Components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  const AUTH_URL =
+    "https://accounts.spotify.com/authorize?client_id=79daaec6f4e74079b6ab2038dd0d8508&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+
+  return (
+    <div>
+      <a href={AUTH_URL}>Login with Spotify</a>
+    </div>
+  );
+};
+
+export default Login;
