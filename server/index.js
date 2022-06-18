94b72395b880fd2dd3d44b321e6bd2fdda6f5aef
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-06-18 17:21:38 +0300

    add cors, body-parser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const SpotifyAPI = require("spotify-web-api-node");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
